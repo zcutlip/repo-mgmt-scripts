552383a4f20d5fdefb3f128d6e5eb0c2e14286f0
Zachary Cutlip <uid000@gmail.com>
2022-07-06 10:05:01 -0700

    add functions.sh

---
+++ src/functions.sh
@@ -0,0 +1,81 @@
+# In shell, success is exit(0), error is anything else, e.g., exit(1)
+SUCCESS=0
+FAILURE=1
+
+quit(){
+    if [ $# -gt 1 ];
+    then
+        echo "$1"
+        shift
+    fi
+    exit "$1"
+
+}
+git_current_branch () {
+	_ref=$(__git_prompt_git symbolic-ref --quiet HEAD 2> /dev/null)
+	_ret=$?
+	if [ $_ret != 0 ]
+	then
+		[ $_ret = 128 ] && return
+		ref=$(__git_prompt_git rev-parse --short HEAD 2> /dev/null)  || return
+	fi
+	echo "${ref#refs/heads/}"
+}
+
+branch_is_master_or_main(){
+    _branch=$(git_current_branch)
+    if [ "$_branch" = "master" ] || [ "$_branch" = "main" ];
+    then
+        return $SUCCESS;
+    else
+        return $FAILURE;
+    fi
+}
+
+branch_is_clean(){
+    _modified=$(git ls-files -m) || quit "Unable to check for modified files." $?
+    if [ -z "$_modified" ];
+    then
+        return $SUCCESS;
+    else
+        return $FAILURE;
+    fi
+}
+
+current_version() {
+    _version="$(python3 ./setup.py --version)" || quit "Unable to detect package version" $?
+    printf "%s" "$_version"
+}
+
+version_is_tagged(){
+    _version="$1"
+    # e.g., verion = 0.1.0
+    # check if git tag -l v0.1.0 exists
+    tag_description=$(git tag -l v"$_version")
+    if [ -n "$tag_description" ];
+    then
+        return $SUCCESS;
+    else
+        return $FAILURE;
+    fi
+}
+
+prompt_yes_no(){
+
+    prompt_string=""
+    if [ -n "$1" ];
+    then
+        prompt_string="$1";
+    fi
+    echo "$prompt_string"
+    read -r response
+
+    case $response in
+    [yY][eE][sS]|[yY])
+        return $SUCCESS
+        ;;
+        *)
+        return $FAILURE
+        ;;
+    esac
+}
